3076b29393d8c733d2e434f48eaf4e54874d71f0
dayamll <dayamllerenacastro@gmail.com>
2018-03-05 00:50:35 -0500

    añadiendo componente Main y estilos

---
+++ src/componentes/Main.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+import "../src/index.css";
+
+class Main extends Component {
+  render() {
+    return (
+       <main className="text">
+      <p><strong>Grace Brewster Murray Hopper</strong> (<a href="https://en.wikipedia.org/wiki/Given_name#Name_at_birth" target="_blank">née</a> <strong>Murray</strong>; December 9, 1906 – January 1, 1992) was an American <a href="https://en.wikipedia.org/wiki/Computer_scientist" target="_blank">computer scientist</a> and <a href="https://en.wikipedia.org/wiki/United_States_Navy" target="_blank">United States Navy rear admiral</a>. <mark>One of the first programmers of the <a href="https://en.wikipedia.org/wiki/Harvard_Mark_I" target="_blank">Harvard Mark I</a> computer </mark>, she was a pioneer of computer programming who invented one of the <a href="#">first compiler</a> related tools. She popularized the idea of machine-independent programming languages, which led to the development of <a href="https://en.wikipedia.org/wiki/COBOL" target="_blank">COBOL</a>, an early <a href="https://en.wikipedia.org/wiki/High-level_programming_language" target="blank_">high-level programming language</a> still in use today.
+		  </p>
+      <p>Hopper had attempted to enlist in the Navy during <a href="https://en.wikipedia.org/wiki/World_War_II" target="_blank">World War II</a>, but she was rejected by the military because she was 34 years of age and too old to enlist. She instead joined the Navy Reserves. Hopper began her computing career when she worked on the Harvard Mark I team that was led by <a href="https://en.wikipedia.org/wiki/Howard_H._Aiken" target="_blank">Howard H. Aiken</a>. In 1949, she joined the <a href="https://en.wikipedia.org/wiki/Eckert%E2%80%93Mauchly_Computer_Corporation" target="_blank">Eckert–Mauchly Computer Corporation</a> and was part of the development team that designed the <a href="https://en.wikipedia.org/wiki/UNIVAC_I" target="_blank">UNIVAC</a> I computer in 1944. It was at Eckert–Mauchly that she began developing the compiler. She believed that computer code could be written in English by using a programming language that was based on English words. The compiler would convert that code into <a href="#">machine code</a> that would be understood by computers. By 1952, Hopper finished her compiler, which was written for the <a href="#">A-0 System</a> programming language.
+		  </p>
+      <p>In 1954, Eckert–Mauchly chose Hopper to lead their department for automatic programming, and she led the release of some of the first compiled languages like <a href="https://en.wikipedia.org/wiki/FLOW-MATIC" target="_blank">FLOW-MATIC</a>. In 1959, she participated in the <a href="https://en.wikipedia.org/wiki/CODASYL" target="_blank">CODASYL</a> consortium, which consulted Hopper to guide them in creating a machine-independent programming language. This led to the <a href="https://en.wikipedia.org/wiki/COBOL" target="_blank">COBOL</a> language, which was inspired by her idea of a language being based on English words. In 1966, she retired from the Naval Reserve, but in 1967, the Navy recalled her to active duty. She retired from the Navy in 1986 and found work as a consultant for the <a href="https://en.wikipedia.org/wiki/Digital_Equipment_Corporation" target="_blank">Digital Equipment Corporation</a>, sharing her computing experiences.
+		  </p>
+      <p>Owing to her accomplishments and her naval rank, she was sometimes referred to as "Amazing Grace". The U.S. Navy <i><a href="https://en.wikipedia.org/wiki/Arleigh_Burke-class_destroyer" target="_blank">Arleigh Burke-class</a></i> guided-missile destroyer <a href="https://en.wikipedia.org/wiki/USS_Hopper" target="_blank">USS <i>Hopper</i></a> was named for her, as was the <a href="https://en.wikipedia.org/wiki/Cray_XE6" target="_blank">Cray XE6</a> "Hopper" supercomputer at <a href="https://en.wikipedia.org/wiki/NERSC" target="_blank">NERSC</a>. During her lifetime, Hopper was awarded 40 honorary degrees from universities across the world. In 1991, she received the <a href="https://en.wikipedia.org/wiki/National_Medal_of_Technology_and_Innovation" target="blank_">National Medal of Technology</a>. On November 22, 2016, she was posthumously awarded the Presidential Medal of Freedom by President <a href="https://en.wikipedia.org/wiki/Barack_Obama" target="_blank" >Barack Obama</a>.
+		  </p>
+      <blockquote>If it's a good idea, go ahead and do it. It's much easier to apologize than it is to get permission.</blockquote>
+      <a href="#" target="_blank">See more</a>
+    </main>
+    );
+  }
+}
+
+export default Main;
